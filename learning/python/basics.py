b2636c6710ae80a37c1cfb26f770434aa6a48376
Vanshika <vanshiiikaa0109@gmsil.com>
2026-06-25 16:06:49 +0530

    add basic.py

---
+++ learning/python/basics.py
@@ -0,0 +1,23 @@
+# Basics of Python
+
+print("Welcome to ML journey")
+
+name = "Vanshika"
+age = 19
+
+print("Name:", name)
+print("Age:", age)
+
+#datatypes
+a = 10
+b = 3.14
+c = "vanshika"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#input from user
+name = input("enter your name")
+print("hello",name)
